d231108013434a3c204754fb3c6ccffa779fd49b
Mushfiqul21 <mushfiqulislam21@gmail.com>
2026-07-21 21:52:50 +0600

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
+function Greetings({name, age, prof})
+{
+  return <h1>Hello, world. I am {name}. I am {age}. I am a {prof}</h1>
+}
 function App() {
 
+  const name = "Mushfiqul Islam";
+  const age = 25;
+  const prof = "Full Stack Developer";
   return (
-    <>
-    
-    </>
+    <div>
+      <Greetings name={name} age={age} prof={prof}/>
+    </div>
   )
 }
 
