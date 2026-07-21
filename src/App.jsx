805f733e4a9d6f0851f168ec23675005c6b99d4e
Mushfiqul21 <mushfiqulislam21@gmail.com>
2026-07-21 22:02:42 +0600

    props, map

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,24 @@ function Greetings({name, age, prof})
 {
   return <h1>Hello, world. I am {name}. I am {age}. I am a {prof}</h1>
 }
+function Tasks()
+{
+  const tasks = [
+    { id: 1, title: "Coding" },
+    { id: 2, title: "Lunch" },
+    { id: 4, title: "Prayer" },
+  ];
+  return (
+    <div>
+ <h2 className='m-2'>My Tasks</h2>
+      <ul className='m-3'>
+        {tasks.map((task, index) => {
+          return <li key={task.id}>{task.title}</li>
+        })}
+      </ul>
+  </div>)
+  
+}
 function App() {
 
   const name = "Mushfiqul Islam";
@@ -12,7 +30,9 @@ function App() {
   const prof = "Full Stack Developer";
   return (
     <div>
-      <Greetings name={name} age={age} prof={prof}/>
+      <Greetings name={name} age={age} prof={prof} />
+      
+      <Tasks />
     </div>
   )
 }
